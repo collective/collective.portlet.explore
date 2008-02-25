f774b7079c406ac279344446e7d95b8ee7d2e36a
Wichert Akkerman <wichert@wiggy.net>
2008-02-25 10:49:12 +0000

    First try to get the portlet context in

--- collective/portlet/explore/kss/expand.py
+++ collective/portlet/explore/kss/expand.py
@@ -3,7 +3,7 @@ from plone.app.kss.plonekssview import PloneKSSView
 
 class ExpandMenu(PloneKSSView):
     @kssaction
-    def expandNode(self, uid):
+    def expandNode(self, portlethash, uid):
         """Expand the navtree at a given UID for a given portlet.
         """
         print uid
