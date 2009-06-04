78587e786a021fa5e1588401b78b58db72dbe07f
Danny Bosan <dpbosan@gmail.com>
2009-06-04 15:24:55 +0000

    first implementation

--- collective/portlet/explore/skins/explorer/explorer.js
+++ collective/portlet/explore/skins/explorer/explorer.js
@@ -12,22 +12,20 @@ jQuery portletNavigationTree plugin
 			
 			// find portlet hash from portlet wrapper
 			var portletWrapper = $(this).closest(".portletWrapper");
-			var portletHash = portletWrapper ? portletWrapper[0].id.replace("portletwrapper-","") : "";
+			
+			var portletHash = portletWrapper[0] ? portletWrapper[0].id.replace("portletwrapper-","") : "";
 			if (!portletHash) return;
 			
 			// observe clicks on toggle buttons
 			$("span.toggleNode", element).live("click", loadNode);
 			$("span.expandedNode", element).live("click", toggleNode);
 			
-			/*
-			Method: toggleNode
-				Expands or collapses a node of which sub-items already have been loaded.
-			*/
+			// Expands or collapses a node of which sub-items already have been loaded.
 			function toggleNode(event){
 				var twistie = $(this);
 				
 				// find ul element
-				var ul = $(this).parents("a").eq(0).next("ul");
+				var ul = $(this).closest("a").next("ul");
 				if (!ul) return;
 				
 				// toggle class names
@@ -43,17 +41,14 @@ jQuery portletNavigationTree plugin
 				event.preventDefault();
 			}
 			
-			/*
-			Method: loadNode
-				Loads the sub-items of a node.
-			*/
+			// Loads the sub-items of a node.
 			function loadNode(event){
 			
 				// prevent default action of event
 				event.preventDefault();
 				
 				// find the li element of the clicked node
-				var node = $(this).parents("li").eq(0);
+				var node = $(this).closest("li");
 				
 				if (node.hasClass("nodeLoading")) return;
 				
