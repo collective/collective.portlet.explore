18c8487018c5015e7c3e997cf98a5f36cacbb4c3
Wichert Akkerman <wichert@wiggy.net>
2009-07-03 08:34:52 +0000

    Restructure logic: move the live event handlers out

--- collective/portlet/explore/skins/explorer/explorer.js
+++ collective/portlet/explore/skins/explorer/explorer.js
@@ -4,6 +4,61 @@ jQuery portletNavigationTree plugin
 */
 
 (function($) {
+	// Expands or collapses a node of which sub-items already have been loaded.
+	function toggleNode(event){
+		var twistie = $(this);
+		
+		// find ul element
+		var ul = $(this).closest("li").children("ul");
+		if (!ul) { return; }
+		
+		// toggle class names
+		if (twistie.hasClass("showChildren")){
+			ul.removeClass("hideChildren");
+			twistie.removeClass("showChildren");
+		} else {
+			ul.addClass("hideChildren");
+			twistie.addClass("showChildren");
+		}
+		
+		// prevent default action of event
+		event.preventDefault();
+	}
+	
+	// Loads the sub-items of a node.
+	function loadNode(event){
+	
+		// prevent default action of event
+		event.preventDefault();
+		
+		// find the li element of the clicked node
+		var node = $(this).closest("li");
+		
+		if (node.hasClass("nodeLoading")) { return; }
+		
+		// get node uid
+		var uidClassName = node[0].className.match(/node-(\w+)/);
+		var uid = uidClassName ? uidClassName[1] : null;
+		if (!uid) { return; }
+		
+		// data to send with request
+		var data = {
+			portlethash : portletHash,
+			uid : uid
+		};
+		
+		// add nodeLoading class
+		node.addClass("nodeLoading");
+		
+		// send request
+		$.post("expandNode", data, function(html){
+			node.replaceWith(html);
+		});
+	}
+
+	// observe clicks on toggle buttons
+	$("span.toggleNode", element).live("click", loadNode);
+	$("span.expandedNode", element).live("click", toggleNode);
 	
 	$.fn.portletNavigationTree = function(options){
 		return this.each(function(){
@@ -16,61 +71,6 @@ jQuery portletNavigationTree plugin
 			var portletHash = portletWrapper[0] ? portletWrapper[0].id.replace("portletwrapper-","") : "";
 			if (!portletHash) { return; }
 			
-			// Expands or collapses a node of which sub-items already have been loaded.
-			function toggleNode(event){
-				var twistie = $(this);
-				
-				// find ul element
-				var ul = $(this).closest("li").children("ul");
-				if (!ul) { return; }
-				
-				// toggle class names
-				if (twistie.hasClass("showChildren")){
-					ul.removeClass("hideChildren");
-					twistie.removeClass("showChildren");
-				} else {
-					ul.addClass("hideChildren");
-					twistie.addClass("showChildren");
-				}
-				
-				// prevent default action of event
-				event.preventDefault();
-			}
-			
-			// Loads the sub-items of a node.
-			function loadNode(event){
-			
-				// prevent default action of event
-				event.preventDefault();
-				
-				// find the li element of the clicked node
-				var node = $(this).closest("li");
-				
-				if (node.hasClass("nodeLoading")) { return; }
-				
-				// get node uid
-				var uidClassName = node[0].className.match(/node-(\w+)/);
-				var uid = uidClassName ? uidClassName[1] : null;
-				if (!uid) { return; }
-				
-				// data to send with request
-				var data = {
-					portlethash : portletHash,
-					uid : uid
-				};
-				
-				// add nodeLoading class
-				node.addClass("nodeLoading");
-				
-				// send request
-				$.post("expandNode", data, function(html){
-					node.replaceWith(html);
-				});
-			}
-
-			// observe clicks on toggle buttons
-			$("span.toggleNode", element).live("click", loadNode);
-			$("span.expandedNode", element).live("click", toggleNode);
 			
 		});
 	};
